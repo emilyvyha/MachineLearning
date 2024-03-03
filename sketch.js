ac230591b2b507bbde8cf042bda44e620baf83b3
Emily Ha <emilyvyha@gmail.com>
2024-03-02 23:33:32 -0600

    changed the UI

--- sketch.js
+++ sketch.js
@@ -6,13 +6,60 @@ let imageModelURL = './';
 // Variables for image classification
 let uploadedImage;
 
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+
 // Load the model first
 function preload() {
     classifier = ml5.imageClassifier(imageModelURL + 'model.json');
 }
 
 function setup() {
-    // Setup canvas if needed
+    createCanvas(320, 260);
+    // Create the video
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
+
+    flippedVideo = ml5.flipImage(video);
+    // Start classifying
+    classifyVideo();
+}
+
+function draw() {
+    background(0);
+    // Draw the video
+    image(flippedVideo, 0, 0);
+
+    // Draw the label
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text(label, width / 2, height - 4);
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+    flippedVideo = ml5.flipImage(video)
+    classifier.classify(flippedVideo, gotResult);
+    flippedVideo.remove();
+}
+
+// When we get a result
+function gotResult(error, results) {
+    // If there is an error
+    if (error) {
+        console.error(error);
+        return;
+    }
+    // The results are in an array ordered by confidence.
+    // console.log(results[0]);
+    label = results[0].label;
+    // Classify again!
+    classifyVideo();
 }
 
 // Function to handle file upload
@@ -32,11 +79,11 @@ function uploadFile() {
 
 // Function to classify the uploaded image
 function classifyImage(img) {
-    classifier.classify(img, gotResult);
+    classifier.classify(img, uploadResult);
 }
 
 // Function to handle the classification result
-function gotResult(error, results) {
+function uploadResult(error, results) {
     let resultDiv = document.getElementById('resultDiv');
     if (error) {
         resultDiv.innerText = "Error: " + error;
